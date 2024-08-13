71d0a3a413b18a892ed5c30a6679b725280052e7
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2024-08-13 18:48:51 +1200

    Start refactoring & splitting tests by "type". Business e2e flows vs isolated features.

---
+++ cypress/e2e/features/login.cy.js
@@ -0,0 +1,9 @@
+describe('Login functionality should work', () => {
+  it('when logging in via UI', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('when logging in via API', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
